3bf347d141ee80824dd68bcb471eb6a272d220d9
Machool Technologies Ltd <jorge.whittembury@machool.com>
2016-12-09 16:51:20 -0800

    v 1.0.0
    - create windows form to select directory

---
+++ GetWeightUSBScale/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScaleReader;
+using System.Threading;
+
+namespace GetWeightUSBScale
+{
+    public partial class Form1 : Form
+    {
+        decimal? weight;
+        bool? isStable;
+        string writeTo = "C:/Users/Machool/AppData/uDoApp";
+
+        USBScale s = new USBScale();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                writeTo = fbd.SelectedPath;
+            }
+
+            s.Connect();
+            textBox1.Text = writeTo;
+            System.Windows.Forms.Timer tt = new System.Windows.Forms.Timer();
+            tt.Tick += new EventHandler(timer_Tick);     // Everytime timer ticks, timer_Tick will be called
+            tt.Interval = 1000;                          // Timer will tick evert second
+            tt.Enabled = true;                           // Enable the timer
+            tt.Start();                                  // Start the timer
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (s.IsConnected)
+            {
+
+                s.GetWeight(out weight, out isStable);
+                //s.DebugScaleData();
+                s.Disconnect();
+                string unit = "Kgs";
+                if (Convert.ToInt16(s.ScaleWeightUnits) == 12) unit = "Lbs";
+                //Console.WriteLine("Weight: {0:0.00} " + unit, weight);
+                string json = "{\"weight\":" + "\"" + weight + "\", \"unit\":\"" + unit + "\"}";
+                System.IO.File.WriteAllText(writeTo + @"/weight.json", json);
+                label2.Text = "logging " + json;
+                label3.Text = "Scale is connected";
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+};
---
+++ GetWeightUSBScale/ScaleInterface.cs
@@ -0,0 +1,126 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidLibrary;
+using System.Threading;
+
+namespace ScaleReader {
+    class USBScale
+    {
+        public bool IsConnected
+        {
+            get
+            {
+                return scale == null ? false : scale.IsConnected;
+            }
+        }
+        public decimal ScaleStatus
+        {
+            get
+            {
+                return inData.Data[1];
+            }
+        }
+        public decimal ScaleWeightUnits
+        {
+            get
+            {
+                return inData.Data[2];
+            }
+        }
+        private HidDevice scale;
+        private HidDeviceData inData;
+
+        public HidDevice[] GetDevices() 
+        { 
+           return HidDevices.Enumerate(0x0eb8, 0xf000).Cast<HidDevice>().ToArray(); 
+        } 
+
+        public bool Connect()
+        {
+            // Find a Scale
+            HidDevice[] deviceList = GetDevices();
+
+            if (deviceList.Length > 0)
+
+                return Connect(deviceList[0]);
+
+            else
+
+                return false;
+        }
+        public bool Connect(HidDevice device)
+        {
+            scale = device;
+            int waitTries = 0;
+            scale.OpenDevice();
+
+            // sometimes the scale is not ready immediatly after
+            // Open() so wait till its ready
+            while (!scale.IsConnected && waitTries < 10)
+            {
+                Thread.Sleep(50);
+                waitTries++;
+            }
+            return scale.IsConnected;
+        }
+        public void Disconnect()
+        {
+            if (scale.IsConnected)
+            {
+                scale.CloseDevice();
+                scale.Dispose();
+            }
+        }
+        public void DebugScaleData()
+        {
+            for (int i = 0; i < inData.Data.Length; ++i)
+            {
+                Console.WriteLine("Byte {0}: {1}", i, inData.Data[i]);
+            }
+        }
+        public void GetWeight(out decimal? weight, out bool? isStable)
+        {
+            weight = null;
+            isStable = false;
+
+            if (scale.IsConnected)
+            {
+                inData = scale.Read(250);
+                // Byte 0 == Report ID?
+                // Byte 1 == Scale Status (1 == Fault, 2 == Stable @ 0, 3 == In Motion, 4 == Stable, 5 == Under 0, 6 == Over Weight, 7 == Requires Calibration, 8 == Requires Re-Zeroing)
+                // Byte 2 == Weight Unit
+                // Byte 3 == Data Scaling (decimal placement)
+                // Byte 4 == Weight LSB
+                // Byte 5 == Weight MSB
+
+                // FIXME: dividing by 100 probably wont work with
+                // every scale, need to figure out what to do with
+                // Byte 3
+                weight = (Convert.ToDecimal(inData.Data[4]) + Convert.ToDecimal(inData.Data[5]) * 256) / 100;
+/*
+                switch (Convert.ToInt16(inData.Data[2]))
+                {
+                    case 3:  // Kilos
+                        //weight = weight * (decimal?)0.453592;
+                        Console.WriteLine("case3");
+                        break;
+                    case 11: // Ounces
+                        //weight = weight * (decimal?)16;
+                        Console.WriteLine("case11");
+                        break;
+                    case 12: // Pounds
+                        // already in pounds, do nothing
+                        Console.WriteLine("case12");
+                        break;
+                }
+ */ 
+                isStable = inData.Data[1] == 0x4;
+            }
+        }
+    }
+}
+
